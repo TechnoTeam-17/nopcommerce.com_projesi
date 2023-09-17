c8162e062c0f6be749e868b9cb438ac7cf3bdd64
NergisRecel <nergisrecel@hotmail.com>
2023-09-17 14:45:10 +0200

    Dataprovider icin object atandi

--- src/US_03_Negatif_Pozitif_Login_Kombinasyonlari.java
+++ src/US_03_Negatif_Pozitif_Login_Kombinasyonlari.java
@@ -1,4 +1,5 @@
 import Utility.BaseDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class US_03_Negatif_Pozitif_Login_Kombinasyonlari extends BaseDriver {
@@ -11,5 +12,16 @@ public class US_03_Negatif_Pozitif_Login_Kombinasyonlari extends BaseDriver {
 
     }
 
-
+    @DataProvider
+    public Object[][] UserData() {
+        Object[][] data = {
+                {"username1", "pw1"},
+                {"username2", "pw2"},
+                {"username3", "pw3"},
+                {"username4", "pw4"},
+                {"username5", "pw5"},
+                {"username6", "pw6"},
+        };
+        return data;
+    }
 }
