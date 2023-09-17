1b2dc3a7505e7c7e61acdbdb7981f66d1cb9a1a5
alpaslanalemdarGEOPETRA <alpaslanalemdar5406@hotmail.com>
2023-09-17 11:13:11 +0200

    Login oluşturuldu. BaseDriver a eklendi.

---
+++ src/US_02_Login.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class US_02_Login {
+    @Test
+    public void login() {
+
+        // Login Fonksiyonu BaserDriver içinde oluşturuldu, EMAİL ve PASSWORD bilgileri registerdan sonra QA Lead'imiz tarafından değiştirilebilir.
+        // Burası sadece bu bilgi için oluşturuldu. BaseDriver çalıştığı sürece Test başarılı olur.
+
+    }
+}
