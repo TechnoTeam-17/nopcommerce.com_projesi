565ad8450756f93303456d85f095a72bc8186b38
NergisRecel <nergisrecel@hotmail.com>
2023-09-17 14:41:20 +0200

    US3 icin class olusturuldu

---
+++ src/US_03_Negatif_Pozitif_Login_Kombinasyonlari.java
@@ -0,0 +1,15 @@
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class US_03_Negatif_Pozitif_Login_Kombinasyonlari extends BaseDriver {
+
+    @Test(dataProvider = "UserData")
+    public void loginkombinasyonlari(){
+
+
+
+
+    }
+
+
+}
