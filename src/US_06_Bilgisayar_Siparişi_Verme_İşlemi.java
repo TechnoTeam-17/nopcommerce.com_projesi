516c6c66aa547f73085bdb485d313c5d0f4dce39
TechnoTeam-17 <142696425+TechnoTeam-17@users.noreply.github.com>
2023-09-19 20:27:39 +0200

    Update US_06_Bilgisayar_Siparişi_Verme_İşlemi.java

--- src/US_06_Bilgisayar_Siparişi_Verme_İşlemi.java
+++ src/US_06_Bilgisayar_Siparişi_Verme_İşlemi.java
@@ -1,5 +1,58 @@
-public class US_06_Bilgisayar_Siparişi_Verme_İşlemi {
-    public static void main(String[] args) {
-        System.out.println("US_06_Bilgisayar_Siparişi_Verme_İşlemi" );
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.List;
+
+
+public class US_06_Bilgisayar_Siparişi_Verme_İşlemi extends BaseDriver {
+@Test()
+    void US_06_Bilgisayar_Siparişi_Verme_İşlemi() {
+
+        driver.get("https://demo.nopcommerce.com/");
+        MyFunc.Bekle(1);
+        Actions actions = new Actions(driver);
+
+        WebElement computers = driver.findElement(By.xpath("(//a[text()='Computers '])[1]"));
+        Action action = actions.moveToElement(computers).build();
+        action.perform();
+        MyFunc.Bekle(1);
+
+        WebElement desktops = driver.findElement(By.xpath("(//a[text()='Desktops '])[1]"));
+        desktops.click();
+        MyFunc.Bekle(1);
+
+        WebElement buildComputer = driver.findElement(By.xpath("//a[text()='Build your own computer']"));
+        buildComputer.click();
+        MyFunc.Bekle(1);
+
+        WebElement ramSelect = wait.until(ExpectedConditions.elementToBeClickable(By.id("product_attribute_2")));
+        action = actions.moveToElement(ramSelect).build();
+        action.perform();
+        MyFunc.Bekle(1);
+
+        List<WebElement> ramOptions = driver.findElements(By.cssSelector("select[id='product_attribute_2']>option"));
+        ramOptions.get((int) (Math.random() * ramOptions.size()-1)+1).click();
+        MyFunc.Bekle(1);
+
+        List<WebElement> hdd = driver.findElements(By.xpath("//input[@name='product_attribute_3']"));
+        hdd.get((int) (Math.random() * hdd.size())).click();
+        MyFunc.Bekle(1);
+
+        WebElement addToCart = driver.findElement(By.id("add-to-cart-button-1"));
+        addToCart.click();
+        MyFunc.Bekle(1);
+
+        WebElement verificationMessage = driver.findElement(By.xpath("//p[text()='The product has been added to your ']"));
+        MyFunc.Bekle(1);
+        Assert.assertEquals(verificationMessage.getText(), "The product has been added to your shopping cart");
+
+        System.out.println(" " +verificationMessage.getText());
+
     }
 }
