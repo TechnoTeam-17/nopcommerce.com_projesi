d59842e9a7abed57f42c0fa765fa928232d27dd7
alpaslanalemdarGEOPETRA <alpaslanalemdar5406@hotmail.com>
2023-09-20 16:55:50 +0300

    NOP DOCUMANINA FINDBY'LAR EKLENDİ, US 7 KISMEN YAPILDI, SON HALİ DEĞİL

--- src/Testler/US_07_SearchByParameter_ParametreliArama.java
+++ src/Testler/US_07_SearchByParameter_ParametreliArama.java
@@ -1,5 +1,44 @@
 package Testler;
 
-public class US_07_SearchByParameter_ParametreliArama {
-    //
+import Utility.BaseDriver;
+import Utility.nopElements;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+
+public class US_07_SearchByParameter_ParametreliArama extends BaseDriver {
+
+    @Test
+    @Parameters("searchText")
+    public void searchTest(String aranacakKelime) {
+
+        nopElements nop=new nopElements();
+
+
+        nop.searchBox.sendKeys(aranacakKelime);
+
+        nop.searchButton.click();
+
+        List<WebElement> productNameList=nop.searchList;
+
+
+        for (WebElement e:productNameList  ) {
+            Assert.assertTrue(e.getText().toLowerCase().contains(aranacakKelime.toLowerCase()), "Eşleşme Sağlanamadı");
+
+        }
+
+
+        //h2[@class='product-title']/a
+
+
+
+        //Adobe Photoshop CS4
+
+
+    }
+
 }
